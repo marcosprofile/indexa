f574d7fd13e6409cdfdfbebdf5aa673b0e5a7445
marcosprofile <marcos_laurindo1997@hotmail.com>
2023-12-31 20:41:47 -0300

    add cabecalho + container

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ContainerComponent } from './components/container/container.component';
+import { CabecalhoComponent } from './components/cabecalho/cabecalho.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    ContainerComponent,
+    CabecalhoComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
