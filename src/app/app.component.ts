fb36172840140719f5bb4135cd728450ed6d9e93
marcosprofile <marcos_laurindo1997@hotmail.com>
2023-12-31 21:08:43 -0300

    Add separador + alfabeto. Update font-family

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { ContainerComponent } from './components/container/container.component';
 import { CabecalhoComponent } from './components/cabecalho/cabecalho.component';
+import { SeparadorComponent } from './components/separador/separador.component';
 
 @Component({
   selector: 'app-root',
@@ -11,11 +12,12 @@ import { CabecalhoComponent } from './components/cabecalho/cabecalho.component';
     CommonModule,
     RouterOutlet,
     ContainerComponent,
-    CabecalhoComponent
+    CabecalhoComponent,
+    SeparadorComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'indexa';
+  alfabeto: string = 'abcdefghijklmnopqrstuvwxyz'
 }
