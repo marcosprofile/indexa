88a429f4f54c368a49171563c45f1ca853c3be6d
marcosprofile <marcos_laurindo1997@hotmail.com>
2024-03-19 19:54:09 -0300

    implementação de validações + integração de rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { ListaContatosComponent } from './paginas/lista-contatos/lista-contatos.component';
+import { FormularioContatoComponent } from './paginas/formulario-contato/formulario-contato.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'lista-contatos',
+    component: ListaContatosComponent
+  },
+  {
+    path: 'formulario',
+    component: FormularioContatoComponent
+  },
+  {
+    path: '',
+    redirectTo: 'lista-contatos',
+    pathMatch: 'full'
+  }
+];
